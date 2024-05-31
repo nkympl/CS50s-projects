882b9f4d4d2a3002d53ddd95e77a6bff3326cd0d
Paula Nakayama <nkympl@hotmail.com>
2024-05-31 18:59:38 -0300

    Add files via upload

---
+++ 3_faces.py
@@ -0,0 +1,24 @@
+"""
+Before there were emoji, there were emoticons, whereby text like :) was a happy face and text like :( was a sad face. 
+Nowadays, programs tend to convert emoticons to emoji automatically!
+
+In a file called faces.py, implement a function called convert that accepts a str as input and returns that same input with any :) converted to 🙂 
+(otherwise known as a slightly smiling face) and any :( converted to 🙁 (otherwise known as a slightly frowning face). 
+All other text should be returned unchanged.
+
+Then, in that same file, implement a function called main that prompts the user for input, calls convert on that input, and prints the result. 
+You are welcome, but not required, to prompt the user explicitly, as by passing a str of your own as an argument to input. 
+Be sure to call main at the bottom of your file.
+"""
+
+def main():
+    userInput = input("What would you like to express? ")
+    userInput = convert(userInput)
+    print(userInput)
+
+def convert(text):
+    text = text.replace(":)", "🙂")
+    text = text.replace(":(", "🙁")
+    return text
+
+main()
---
+++ 4_einstein.py
@@ -0,0 +1,7 @@
+"""
+In a file called einstein.py, implement a program in Python that prompts the user for mass as an integer (in kilograms) and then outputs the equivalent 
+number of Joules as an integer. Assume that the user will input an integer.
+"""
+
+m = int(input("m: "))
+print("E: ", m * 30000000 ** 2)
